939ed6632f554318317787bec027c4e867a7b324
Rsho11 <kxcrox89@gmail.com>
2025-07-28 18:37:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,77 +2,44 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = process.env.PORT || 3000;
+
+const SAVE_PATH = path.join(__dirname, 'saves');
+if (!fs.existsSync(SAVE_PATH)) fs.mkdirSync(SAVE_PATH);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// In-memory player store
 let players = {};
 
 io.on('connection', (socket) => {
-  const defaultPlayer = {
-    id: socket.id,
-    name: 'Dreamer',
-    color: '#' + Math.floor(Math.random()*16777215).toString(16),
-    currentRoom: 'WaitingRoom',
-    position: { x: 200, y: 200 },
-    abilities: []
-  };
-  players[socket.id] = defaultPlayer;
+    const id = socket.id;
+    const saveFile = path.join(SAVE_PATH, `${id}.json`);
+    players[id] = { name: 'Dreamer', abilities: [], visited: [] };
 
-  socket.emit('init', { id: socket.id, player: defaultPlayer, players });
-
-  socket.broadcast.emit('playerJoined', { id: socket.id, player: defaultPlayer });
-
-  socket.on('setName', (name) => {
-    if (players[socket.id]) {
-      players[socket.id].name = name;
-      io.emit('nameUpdated', { id: socket.id, name });
+    if (fs.existsSync(saveFile)) {
+        players[id] = JSON.parse(fs.readFileSync(saveFile));
     }
-  });
 
-  socket.on('move', (position) => {
-    if (players[socket.id]) {
-      players[socket.id].position = position;
-      io.emit('playerMoved', { id: socket.id, position });
-    }
-  });
+    socket.emit('loadPlayer', players[id]);
 
-  socket.on('chat message', (msg) => {
-    const player = players[socket.id];
-    if (player) {
-      io.emit('chat message', { name: player.name, message: msg, room: player.currentRoom });
-    }
-  });
+    socket.on('updatePlayer', (data) => {
+        players[id] = data;
+        fs.writeFileSync(saveFile, JSON.stringify(data));
+    });
 
-  socket.on('typing', (isTyping) => {
-    io.emit('typing', { id: socket.id, isTyping });
-  });
-
-  socket.on('enterDream', () => {
-    if (players[socket.id]) {
-      players[socket.id].currentRoom = 'SpiralStaircase';
-      io.emit('roomChanged', { id: socket.id, room: 'SpiralStaircase', player: players[socket.id] });
-    }
-  });
-
-  socket.on('changeRoom', (room) => {
-    if (players[socket.id]) {
-      players[socket.id].currentRoom = room;
-      io.emit('roomChanged', { id: socket.id, room, player: players[socket.id] });
-    }
-  });
-
-  socket.on('disconnect', () => {
-    delete players[socket.id];
-    io.emit('playerLeft', { id: socket.id });
-  });
+    socket.on('disconnect', () => {
+        delete players[id];
+    });
 });
 
-server.listen(PORT, () => console.log(`Knitting in the Dark server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Knitting in the Dark running on port ${PORT}`));

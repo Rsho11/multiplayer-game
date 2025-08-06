a368ef3e450d02d117d64092031d154c02cb542f
Rsho11 <kxcrox89@gmail.com>
2025-08-06 18:22:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
-app.use('/models', express.static('models'));
+
+let players = {};
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+    console.log(`Player connected: ${socket.id}`);
+    players[socket.id] = { x: 0, y: 0, z: 0 };
+
+    socket.emit("currentPlayers", players);
+    socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
+
+    socket.on("move", (pos) => {
+        players[socket.id] = pos;
+        socket.broadcast.emit("playerMoved", { id: socket.id, ...pos });
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`Player disconnected: ${socket.id}`);
+        delete players[socket.id];
+        io.emit("removePlayer", socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });

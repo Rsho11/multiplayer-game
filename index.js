219fd6268084de98899a1cc7026cd1e0662e175d
Rsho11 <kxcrox89@gmail.com>
2025-08-06 18:59:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Serve static files
 app.use(express.static("public"));
 app.use("/models", express.static("public/models"));
 
-const players = {}; // id -> { x,y,z, name, color }
+// id -> { x,y,z, name, color }
+const players = {};
 
 io.on("connection", (socket) => {
   console.log("connected:", socket.id);
 
-  // wait for client to send name & color before adding them
+  // Client will send name & color before we add them to the world
   socket.on("register", ({ name, color }) => {
     players[socket.id] = { x: 0, y: 0, z: 0, name, color };
 
-    // send current roster to the newcomer
+    // Send the roster to newcomer
     socket.emit("currentPlayers", { players, you: socket.id });
 
-    // tell others about the new player
+    // Tell others about the join
     socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
   });
 
   socket.on("move", (pos) => {
     const p = players[socket.id];
-    if (!p) return;
+    if (!p) return; // ignore until registered
     players[socket.id] = { ...p, ...pos };
     socket.broadcast.emit("playerMoved", { id: socket.id, ...players[socket.id] });
   });

c1f5fb4f281a84bea880674c32b6d1d51ad693b7
Rsho11 <kxcrox89@gmail.com>
2025-08-13 11:03:16 +0300

    feat: add chat and social features

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use("/models", express.static("public/models"));
 
 // player store: id -> { x,y,z, name, color, isGuest, google: {sub,name,picture} }
 const players = {};
+const friends = {}; // id -> Set of friend ids
 
 io.on("connection", (socket) => {
 
@@ -56,8 +57,37 @@ io.on("connection", (socket) => {
     io.emit("chat", { id: socket.id, text: String(txt||"").slice(0,160) });
   });
 
+  socket.on("typing", (flag) => {
+    socket.broadcast.emit("typing", { id: socket.id, typing: !!flag });
+  });
+
+  socket.on("friendRequest", ({ to }) => {
+    if (!players[to]) return;
+    io.to(to).emit("friendRequest", { from: socket.id, name: players[socket.id]?.name });
+  });
+
+  socket.on("friendAccept", ({ from }) => {
+    if (!players[from]) return;
+    friends[socket.id] = friends[socket.id] || new Set();
+    friends[from] = friends[from] || new Set();
+    friends[socket.id].add(from);
+    friends[from].add(socket.id);
+    io.to(socket.id).emit("friendAccepted", { id: from, name: players[from]?.name });
+    io.to(from).emit("friendAccepted", { id: socket.id, name: players[socket.id]?.name });
+  });
+
+  socket.on("privateChat", ({ to, text }) => {
+    if (friends[socket.id]?.has(to)) {
+      const msg = String(text||"").slice(0,160);
+      io.to(to).emit("privateChat", { id: socket.id, text: msg });
+      socket.emit("privateChat", { id: socket.id, text: msg });
+    }
+  });
+
   socket.on("disconnect", () => {
     delete players[socket.id];
+    delete friends[socket.id];
+    Object.values(friends).forEach((set) => set.delete(socket.id));
     io.emit("removePlayer", socket.id);
   });
 });
